bbd99d7638b57b52cfa011296f13c2e9b6a542a4
Sahil Sharma <sahilsharma2184@gmail.com>
2024-03-19 18:47:11 +0530

    Update superhero.js

--- superhero.js
+++ superhero.js
@@ -1,5 +1,6 @@
 /*Make sure to add 'type=module' in the package.json file else the import won't work
-The code gives a random superhero name from the superheroes npm module*/
+The code gives a random superhero name from the superheroes npm module
+Get to know more about this module at https://www.npmjs.com/package/superheroes */
 import superheroes from "superheroes";
 const name=superheroes.random();
 console.log(`My name is ${name}!`);
