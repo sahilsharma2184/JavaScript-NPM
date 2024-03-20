703036b4d4e423f9a106b9a3dcc4328e59c85e99
Sahil Sharma <sahilsharma2184@gmail.com>
2024-03-20 14:37:48 +0530

    Create sillyname.js

---
+++ sillyname.js
@@ -0,0 +1,7 @@
+/*
+Sillyname NPM module generates a random sillyname
+Get to know more about this module on https://www.npmjs.com/package/sillyname
+*/
+import generateName from "sillyname";
+var sillyname=generateName();
+console.log("The random sillyname generated is ${sillyname}");
