6ba02fd08f67587f4843d49ff21a4b8d5b4db3df
Sahil Sharma <sahilsharma2184@gmail.com>
2024-03-20 14:41:31 +0530

    Update sillyname.js

--- sillyname.js
+++ sillyname.js
@@ -4,4 +4,4 @@ Get to know more about this module on https://www.npmjs.com/package/sillyname
 */
 import generateName from "sillyname";
 var sillyname=generateName();
-console.log("The random sillyname generated is ${sillyname}");
+console.log(`My name is ${sillyname}`);
